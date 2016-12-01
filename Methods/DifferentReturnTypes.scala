593461c20f50f91aebde9d6d3a444d185568555f
Anup-Unawane <anup.unawane@gmail.com>
2016-12-02 03:29:58 +0530

    Create DifferentReturnTypes.scala
    Different Return Types demo

---
+++ Methods/DifferentReturnTypes.scala
@@ -0,0 +1,37 @@
+/* Here return type is different in if and else blocks
+So scala will infer return type as Any.
+
+scala.Any is super class of all types in scala
+scala.AnyVal and scala.AnyRef are subclasses of scala.Any
+
+scala.AnyVal is superclass for all primitive types in scala like Byte, Int, Short, Char, Int, Boolean, Double, Float
+scala.AnyRef is superclass for all other types. It is analogous to java.lang.Object
+
+ **/
+
+def add(x:Int, y:Int) = if(x > 10) (x+y).toString else x + y
+
+println(add(11 , 2))
+
+
+/** 
+Sample Output:
+
+E:\Courses\scala\scripts>scala DifferentReturnTypes.scala
+13
+
+E:\Courses\scala\scripts>scala
+Welcome to Scala 2.11.8 (Java HotSpot(TM) 64-Bit Server VM, Java 1.8.0_92).
+Type in expressions for evaluation. Or try :help.
+
+scala> :paste
+// Entering paste mode (ctrl-D to finish)
+
+def add(x:Int, y:Int) = if(x > 10) (x+y).toString else x + y
+
+// Exiting paste mode, now interpreting.
+
+add: (x: Int, y: Int)Any
+
+scala> :q
+*/
