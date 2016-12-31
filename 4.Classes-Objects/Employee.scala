32ec1a8f0a24294826110883bc71683084248aca
Anup-Unawane <anup.unawane@gmail.com>
2016-12-31 13:52:50 +0530

    Update Employee.scala
    Added another parameter to default constructor.
    Added another constructor with 2 parameters

--- 4.Classes-Objects/Employee.scala
+++ 4.Classes-Objects/Employee.scala
@@ -3,8 +3,12 @@
   * var creates both accessors and mutators(setters)
   * you can view class file using javap -p <class name>
   *
+  *   call another constructor by invoking this
+  *
   */
-class Employee (val firstName:String, var lastName:String)
+class Employee (val firstName:String, var lastName:String, val title:String)      //This is default constructor
 {
-
+  //Another constructor
+  def this(firstName:String, lastName:String ) = {this(firstName, lastName, "Programmer")     //if constructor block is multiline, First call must be this
+                                                  println("Multiline constructor block!!")}
 }
