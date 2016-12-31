2ad52b7529d71abed6bab9765ffd29ed8a2c8e69
Anup-Unawane <anup.unawane@gmail.com>
2016-12-31 12:00:28 +0530

    Create Employee.scala

---
+++ 4.Classes-Objects/Employee.scala
@@ -0,0 +1,10 @@
+/**
+  * val creates accessors(getters), access to inner state
+  * var creates both accessors and mutators(setters)
+  * you can view class file using javap -p <class name>
+  *
+  */
+class Employee (val firstName:String, var lastName:String)
+{
+
+}
