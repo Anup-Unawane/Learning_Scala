d985a0dc2a1e97360f621700dcccf09a0709885b
Anup-Unawane <anup.unawane@gmail.com>
2016-12-31 12:04:07 +0530

    Create EmployeeScript.scala

---
+++ 4.Classes-Objects/EmployeeScript.scala
@@ -0,0 +1,45 @@
+val emp1 = new Employee("Gabbar", "Singh")
+println("FirstName:" + emp1.firstName)
+println("LastName:" + emp1.lastName)
+
+emp1.lastName = "Babbar"
+println("Modified LastName :" + emp1.lastName)
+
+/**
+  *Sample output
+  *
+******Before adding var and val to class params
+E:\Courses\scala\IntellijIDEA\src>scalac Employee.scala
+
+E:\Courses\scala\IntellijIDEA\src>javap -p Employee.class
+Compiled from "Employee.scala"
+public class Employee {
+  public Employee(java.lang.String, java.lang.String);
+}
+
+  ******After adding var and val to class params
+E:\Courses\scala\IntellijIDEA\src>scalac Employee.scala
+
+E:\Courses\scala\IntellijIDEA\src>javap -p Employee.class
+Compiled from "Employee.scala"
+public class Employee {
+  private final java.lang.String firstName;
+  private java.lang.String lastName;
+  public java.lang.String firstName();
+  public java.lang.String lastName();
+  public void lastName_$eq(java.lang.String);
+  public Employee(java.lang.String, java.lang.String);
+}
+
+E:\Courses\scala\IntellijIDEA\src>scala -cp . EmployeeScript.scala
+FirstName:Gabbar
+Modified LastName :Babbar
+
+E:\Courses\scala\IntellijIDEA\src>scala -cp . EmployeeScript.scala
+FirstName:Gabbar
+LastName:Singh
+Modified LastName :Babbar
+
+E:\Courses\scala\IntellijIDEA\src>
+
+  */
