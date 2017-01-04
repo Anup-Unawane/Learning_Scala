7688b5799efbde126fcb69dd3f98745d0c39e524
Anup-Unawane <anup.unawane@gmail.com>
2017-01-04 09:02:15 +0530

    Update Employee.scala
    Added validation for arguments

--- 4.Classes-Objects/Employee.scala
+++ 4.Classes-Objects/Employee.scala
@@ -1,3 +1,5 @@
+import scala.beans.BeanProperty
+
 /**
   * val creates accessors(getters), access to inner state
   * var creates both accessors and mutators(setters)
@@ -6,8 +8,15 @@
   *   call another constructor by invoking this
   *
   */
-class Employee (val firstName:String, var lastName:String, val title:String)      //This is default constructor
+class Employee (@BeanProperty val firstName:String, @BeanProperty var lastName:String, @BeanProperty val title:String)      //This is default constructor
 {
+  require(firstName.nonEmpty, "First Name cannot be empty!!")     //preconditions using require
+  require(lastName.nonEmpty, "Last Name cannot be empty!!")
+  require(title.nonEmpty, "Title cannot be empty!!")
+
+  if(title.contains("Senior") || title.contains("Junior"))    //Preconditions using manual check
+    throw new IllegalArgumentException("Title cannot contain Senior or Junior!!")
+
   //Another constructor
   def this(firstName:String, lastName:String ) = {this(firstName, lastName, "Programmer")     //if constructor block is multiline, First call must be this
                                                   println("Multiline constructor block!!")}
